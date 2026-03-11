96923f3f79dd61d9ca34b572e30936d8e02283c4
HaNaKo <bassene9699@gmail.com>
2026-03-11 14:46:55 +0000

    add mui datagrid

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,50 +1,20 @@
-import { useState } from "react";
 import Sidebar from "./Sidebar";
 import Header from "./Header";
 import { Outlet } from "react-router-dom";
-import { Drawer, useMediaQuery } from "@mui/material";
 
 const Layout = () => {
 
-  const [open, setOpen] = useState(false);
-
-  const isMobile = useMediaQuery("(max-width:768px)");
-
   return (
 
-    <div style={{ display: "flex", minHeight: "100vh" }}>
+    <div style={{ display: "flex" }}>
 
-      {/* Sidebar desktop */}
-      {!isMobile && (
-        <div style={{ width: "220px", flexShrink: 0 }}>
-          <Sidebar />
-        </div>
-      )}
-
-      {/* Sidebar mobile */}
-      {isMobile && (
-        <Drawer
-          open={open}
-          onClose={() => setOpen(false)}
-        >
-          <Sidebar />
-        </Drawer>
-      )}
+      <Sidebar />
 
       <div style={{ flex: 1 }}>
 
-        <Header
-          toggleMenu={() => setOpen(true)}
-          isMobile={isMobile}
-        />
+        <Header />
 
-        <div
-          style={{
-            padding: "20px",
-            maxWidth: "100%",
-            overflowX: "auto"
-          }}
-        >
+        <div style={{ padding: "20px" }}>
           <Outlet />
         </div>
 
